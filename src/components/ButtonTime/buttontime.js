66f2b33fc69318c702c84937ef3493bb779f8e0a
Ilnur Saifutdinov <ilnursoft@gmail.com>
2018-09-06 11:17:18 +0300

    ButtonTime tag create handleClickSelect

--- src/components/ButtonTime/buttontime.js
+++ src/components/ButtonTime/buttontime.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import './buttontime.css'
 
+var classNames = require('classnames');
+
 class ButtonTime extends Component {
     state = { 
         selected: false // true - дата выбрана и кнопка выбора исчезает
-     }
+    }
+
+    handleClickSelect = event => {
+        this.setState({selected: true});
+    }
+
+
     render() { 
         const {hour, minute} = this.props;
         const selected = this.state.selected;
         return ( 
-            <div className="buttontime">
+            <div className={classNames("buttontime", {"buttontime-selected":selected})}>
                 {hour}:{minute}
-                {selected?(<div></div>):(<div>+</div>)}
+                {selected?(<div></div>):(<div onClick={this.handleClickSelect}>+</div>)}
             </div>
-         );
+         );    
     }
 }
  
