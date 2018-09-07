deae56d8ced13183f92fa6b14d1ff9da6c6e4d11
Ilnur Saifutdinov <ilnursoft@gmail.com>
2018-09-07 12:43:29 +0300

    add back and forward week in month

--- ex.js
+++ ex.js
@@ -1,5 +1,5 @@
 var moment = require("moment");
-
+/*
 console.info(
   moment()
     .month(13)
@@ -13,5 +13,35 @@ console.info(
 );
 
 console.info(moment("11-9-2018", "DD-MM-YYYY").format("d"));
+*/
 
-console.info(moment("1-12-2018", "DD-MM-YYYY").isoWeekday()); // определяет день недели
+console.info(moment("7-9-2018", "DD-MM-YYYY").isoWeekday()); // определяет день недели
+
+
+const generateMonth = (month, year) => {
+  const dates = year.toString() +"-"+month.toString();  
+  const dayMonth = moment(dates, "YYYY-MM").daysInMonth();
+  console.info(dates);
+  console.info(dayMonth);
+  let calendarMonth = [];
+  numWeek = -1;
+  for (let index = 1; index < dayMonth+1; index++) {
+    const dateCurrent =  dates +"-" + index.toString();
+    const dayWeek = moment(dateCurrent, "YYYY-MM-DD").isoWeekday();
+    if (numWeek === -1) {
+      calendarMonth.push([0,0,0,0,0,0,0]);
+      numWeek = numWeek + 1;
+      calendarMonth[numWeek][dayWeek-1] = index;
+    } else {
+      if (dayWeek === 1) {
+        calendarMonth.push([0,0,0,0,0,0,0]);
+        numWeek = numWeek + 1;
+      }
+      calendarMonth[numWeek][dayWeek-1] = index;
+    }
+  }
+  return calendarMonth;
+}
+
+console.info(generateMonth(9,2018));
+
--- src/components/ButtonWeekDay/buttonweekday.js
+++ src/components/ButtonWeekDay/buttonweekday.js
@@ -4,27 +4,88 @@ import "./buttonweekday.css";
 var classNames = require("classnames");
 var moment = require("moment");
 
+const generateMonth = (month, year) => {
+  const dates = year.toString() +"-"+month.toString();  
+  const dayMonth = moment(dates, "YYYY-MM").daysInMonth();
+  console.info(dates);
+  console.info(dayMonth);
+  let calendarMonth = [];
+  let numWeek = -1;
+  for (let index = 1; index < dayMonth+1; index++) {
+    const dateCurrent =  dates +"-" + index.toString();
+    const dayWeek = moment(dateCurrent, "YYYY-MM-DD").isoWeekday();
+    if (numWeek === -1) {
+      calendarMonth.push([0,0,0,0,0,0,0]);
+      numWeek = numWeek + 1;
+      calendarMonth[numWeek][dayWeek-1] = index;
+    } else {
+      if (dayWeek === 1) {
+        calendarMonth.push([0,0,0,0,0,0,0]);
+        numWeek = numWeek + 1;        
+      }
+      calendarMonth[numWeek][dayWeek-1] = index;
+    }
+  }
+  return calendarMonth;
+}
+
+
 class ButtonWeekDay extends Component {
-  state = {
-    days: [1, 2, 3, 4, 5],
-    week: 1,
-    month: 0,
-    year: 2018
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: [],
+      week: 0,
+      month: 8,
+      year: 2018
+    };
+
+    this.state.days = generateMonth(this.state.month, this.state.year);
+
+    this.state.week = this.checkEmpltyMondayFriday(this.state.days[this.state.week])?this.state.week+1:this.state.week;
+
+  }
+
+  checkEmpltyMondayFriday = (weekday) => {
+    /* true - если ПН до ПТ пустые дни */
+    if (weekday[0]+weekday[1]+weekday[2]+weekday[3]+weekday[4] ===0) {
+      return true;
+    }
+    return false;
+  }
+
+  handleBackMonth = event => {
+    var currentWeek = this.state.week - 1;
+
+    currentWeek = this.checkEmpltyMondayFriday(this.state.days[currentWeek])?currentWeek+1:currentWeek;
+
+    this.setState({week: currentWeek<0?0:currentWeek});
+  }
+
+  handleForwardMonth = event => {
+    var currentWeek = this.state.week + 1;
+    const countWeek = this.state.days.length;
+
+    this.setState({week: currentWeek<countWeek?currentWeek:countWeek-1});
+  }
 
   render() {
     return (
       <div className="buttonweekday">
-        <div className="buttonweekday-arrow">&larr;</div>
-        <div className="buttonweekday-day">{this.state.days[0]} Monday</div>
-        <div className="buttonweekday-day">{this.state.days[1]} Tuesday</div>
-        <div className="buttonweekday-day">{this.state.days[2]} Wednesday</div>
-        <div className="buttonweekday-day">{this.state.days[3]} Thursday</div>
-        <div className="buttonweekday-day">{this.state.days[4]} Friday</div>
-        <div className="buttonweekday-arrow">&rarr;</div>
+        <div className="buttonweekday-arrow" onClick={this.handleBackMonth}>&larr;</div>
+        {console.info(this.state.week)}
+        {console.info(this.state.days)}
+        <div className="buttonweekday-day">{this.state.days[this.state.week][0]} Monday</div>
+        <div className="buttonweekday-day">{this.state.days[this.state.week][1]} Tuesday</div>
+        <div className="buttonweekday-day">{this.state.days[this.state.week][2]} Wednesday</div>
+        <div className="buttonweekday-day">{this.state.days[this.state.week][3]} Thursday</div>
+        <div className="buttonweekday-day">{this.state.days[this.state.week][4]} Friday</div>
+        <div className="buttonweekday-arrow" onClick={this.handleForwardMonth}>&rarr;</div>
       </div>
     );
   }
 }
 
+
 export default ButtonWeekDay;
+
