374c95c14e89fdbb5c4c1c0e52c97e6764ed5e64
Ilnur Saifutdinov <ilnursoft@gmail.com>
2018-09-13 23:27:44 +0300

    fix ButtonWeekDay and ButtonMontn

--- src/components/ButtonWeekDay/buttonweekday.js
+++ src/components/ButtonWeekDay/buttonweekday.js
@@ -4,89 +4,63 @@ import "./buttonweekday.css";
 var classNames = require("classnames");
 var moment = require("moment");
 
-const generateMonth = (month, year) => {
-  const dates = year.toString() +"-"+month.toString();  
-  const dayMonth = moment(dates, "YYYY-MM").daysInMonth();
-  console.info(dates);
-  console.info(dayMonth);
-  let calendarMonth = [];
-  let numWeek = -1;
-  for (let index = 1; index < dayMonth+1; index++) {
-    const dateCurrent =  dates +"-" + index.toString();
-    const dayWeek = moment(dateCurrent, "YYYY-MM-DD").isoWeekday();
-    if (numWeek === -1) {
-      calendarMonth.push([0,0,0,0,0,0,0]);
-      numWeek = numWeek + 1;
-      calendarMonth[numWeek][dayWeek-1] = index;
-    } else {
-      if (dayWeek === 1) {
-        calendarMonth.push([0,0,0,0,0,0,0]);
-        numWeek = numWeek + 1;        
-      }
-      calendarMonth[numWeek][dayWeek-1] = index;
-    }
+const checkEmpltyMondayFriday = function(weekday) {
+  /* true - если ПН до ПТ пустые дни */
+  if (weekday[0] + weekday[1] + weekday[2] + weekday[3] + weekday[4] === 0) {
+    return true;
   }
-  return calendarMonth;
-}
-
+  return false;
+};
 
 class ButtonWeekDay extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      days: [],
-      week: this.props.week === undefined ? 0 : parseInt(this.props.week),
-      month: this.props.month === undefined ? 0 : parseInt(this.props.month),
-      year: this.props.year === undefined ? 2018 : parseInt(this.props.year)
-    };
-
-    this.state.days = generateMonth(this.state.month+1, this.state.year);
-
-    this.state.week = this.checkEmpltyMondayFriday(this.state.days[this.state.week])?this.state.week+1:this.state.week;
-
-  }
-
-  checkEmpltyMondayFriday = (weekday) => {
-    /* true - если ПН до ПТ пустые дни */
-    if (weekday[0]+weekday[1]+weekday[2]+weekday[3]+weekday[4] ===0) {
-      return true;
-    }
-    return false;
-  }
-
   handleBackMonth = event => {
-    var currentWeek = this.state.week - 1;
+    var currentWeek = this.props.week - 1;
 
-    currentWeek = currentWeek<0?0:currentWeek;
-    currentWeek = this.checkEmpltyMondayFriday(this.state.days[currentWeek])?currentWeek+1:currentWeek;
+    currentWeek = currentWeek < 0 ? 0 : currentWeek;
+    currentWeek = checkEmpltyMondayFriday(this.props.days[currentWeek])
+      ? currentWeek + 1
+      : currentWeek;
 
-    this.setState({week: currentWeek});
-  }
+    this.props.onChangeWeek(currentWeek);
+  };
 
   handleForwardMonth = event => {
-    var currentWeek = this.state.week + 1;
-    const countWeek = this.state.days.length;
+    var currentWeek = this.props.week + 1;
+    const countWeek = this.props.days.length;
+    currentWeek = currentWeek < countWeek ? currentWeek : countWeek - 1;
 
-    this.setState({week: currentWeek<countWeek?currentWeek:countWeek-1});
-  }
+    this.props.onChangeWeek(currentWeek);
+  };
 
   render() {
     return (
       <div className="buttonweekday">
-        <div className="buttonweekday-arrow" onClick={this.handleBackMonth}>&larr;</div>
-        {console.info(this.state.week)}
-        {console.info(this.state.days)}
-        <div className="buttonweekday-day">{this.state.days[this.state.week][0]} Monday</div>
-        <div className="buttonweekday-day">{this.state.days[this.state.week][1]} Tuesday</div>
-        <div className="buttonweekday-day">{this.state.days[this.state.week][2]} Wednesday</div>
-        <div className="buttonweekday-day">{this.state.days[this.state.week][3]} Thursday</div>
-        <div className="buttonweekday-day">{this.state.days[this.state.week][4]} Friday</div>
-        <div className="buttonweekday-arrow" onClick={this.handleForwardMonth}>&rarr;</div>
+        <div className="buttonweekday-arrow" onClick={this.handleBackMonth}>
+          &larr;
+        </div>
+        {console.info(this.props.week)}
+        {console.info(this.props.days)}
+        <div className="buttonweekday-day">
+          {this.props.days[this.props.week][0]} Monday
+        </div>
+        <div className="buttonweekday-day">
+          {this.props.days[this.props.week][1]} Tuesday
+        </div>
+        <div className="buttonweekday-day">
+          {this.props.days[this.props.week][2]} Wednesday
+        </div>
+        <div className="buttonweekday-day">
+          {this.props.days[this.props.week][3]} Thursday
+        </div>
+        <div className="buttonweekday-day">
+          {this.props.days[this.props.week][4]} Friday
+        </div>
+        <div className="buttonweekday-arrow" onClick={this.handleForwardMonth}>
+          &rarr;
+        </div>
       </div>
     );
   }
 }
 
-
-export default ButtonWeekDay;
-
+export { ButtonWeekDay, checkEmpltyMondayFriday };
--- src/components/PanelWeekDay/panelweekday.js
+++ src/components/PanelWeekDay/panelweekday.js
@@ -1,20 +1,64 @@
 import React, { Component } from "react";
 import "./panelweekday.css";
 import ButtonMonth from "../ButtonMonth/buttonmonth";
-import ButtonWeekDay from "../ButtonWeekDay/buttonweekday";
+import {
+  ButtonWeekDay,
+  checkEmpltyMondayFriday
+} from "../ButtonWeekDay/buttonweekday";
 
 var classNames = require("classnames");
 var moment = require("moment");
 
+const generateMonth = (month, year) => {
+  const dates = year.toString() + "-" + month.toString();
+  const dayMonth = moment(dates, "YYYY-MM").daysInMonth();
+  console.info(dates);
+  console.info(dayMonth);
+  let calendarMonth = [];
+  let numWeek = -1;
+  for (let index = 1; index < dayMonth + 1; index++) {
+    const dateCurrent = dates + "-" + index.toString();
+    const dayWeek = moment(dateCurrent, "YYYY-MM-DD").isoWeekday();
+    if (numWeek === -1) {
+      calendarMonth.push([0, 0, 0, 0, 0, 0, 0]);
+      numWeek = numWeek + 1;
+      calendarMonth[numWeek][dayWeek - 1] = index;
+    } else {
+      if (dayWeek === 1) {
+        calendarMonth.push([0, 0, 0, 0, 0, 0, 0]);
+        numWeek = numWeek + 1;
+      }
+      calendarMonth[numWeek][dayWeek - 1] = index;
+    }
+  }
+  return calendarMonth;
+};
+
 class PanelWeekDay extends Component {
-  state = {
-    week: this.props.week === undefined ? 0 : parseInt(this.props.week),
-    month: this.props.month === undefined ? 0 : parseInt(this.props.month), // 0 -> January, 1 ->Febriary ...
-    year: this.props.year === undefined ? 2018 : parseInt(this.props.year)
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      week: this.props.week === undefined ? 0 : parseInt(this.props.week),
+      month: this.props.month === undefined ? 0 : parseInt(this.props.month), // 0 -> January, 1 ->Febriary ...
+      year: this.props.year === undefined ? 2018 : parseInt(this.props.year),
+      days: []
+    };
+    this.state.days = generateMonth(this.state.month + 1, this.state.year);
+
+    this.state.week = checkEmpltyMondayFriday(this.state.days[this.state.week])
+      ? this.state.week + 1
+      : this.state.week;
+  }
 
   onChangeMonth = newMonth => {
     this.setState({ month: newMonth });
+
+    this.setState({
+      days: generateMonth(this.state.month + 1, this.state.year)
+    });
+    this.setState({
+      week: checkEmpltyMondayFriday(this.state.days[0]) ? 1 : 0
+    });
   };
 
   onChangeWeek = newWeek => {
@@ -33,6 +77,7 @@ class PanelWeekDay extends Component {
           month={this.state.month}
           year={this.state.year}
           week={this.state.week}
+          days={this.state.days}
           onChangeWeek={this.onChangeWeek}
         />
       </div>
